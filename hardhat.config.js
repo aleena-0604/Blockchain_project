968b93f328142cde2b0871ea31366abf817e22ed
aleena-0604 <aleenabprem.com>
2024-11-08 12:44:51 +0530

    add deploy

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,11 +1,22 @@
+require("@nomicfoundation/hardhat-toolbox");
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+  defaultNetwork: "localhost",
+  networks: {
+    localhost: {
+      url: "http://127.0.0.1:8545"
+    },
+    hardhat: {
+      // See its defaults
+    }
+  },
   solidity: {
     version: "0.8.20",
     settings: {
       optimizer: {
         enabled: true,
-        runs: 1000,
+        runs: 1,
       },
     },
   },
